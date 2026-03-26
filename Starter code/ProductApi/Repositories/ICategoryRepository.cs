fe82c9ffea0c6fe6327e9ba671e1faa5a7b73ac7
Viljami Mansikka <gvima010@edu.xamk.fi>
2026-03-26 13:40:05 +0200

    Valmis tehtävä

---
+++ Starter code/ProductApi/Repositories/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using ProductApi.Models;
+
+namespace ProductApi.Repositories;
+
+public interface ICategoryRepository
+{
+    Task<List<Category>> GetAllAsync();
+    Task<Category?> GetByIdAsync(int id);
+    Task<Category> AddAsync(Category category);
+    Task<bool> DeleteAsync(int id);
+}
---
+++ Starter code/ProductApi/Services/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using ProductApi.Common;
+using ProductApi.Models.Dtos;
+
+namespace ProductApi.Services;
+
+public interface ICategoryService
+{
+    Task<Result<List<CategoryResponse>>> GetAllAsync();
+    Task<Result<CategoryResponse>> GetByIdAsync(int id);
+    Task<Result<CategoryResponse>> CreateAsync(CreateCategoryRequest request);
+    Task<Result> DeleteAsync(int id);
+}
